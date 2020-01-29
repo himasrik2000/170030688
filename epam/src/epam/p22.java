b4ec3a6ffbde38024881b6d6f72eb01257c990cc
HELLO <HELLO@DESKTOP-Q1BUVP4>
2020-01-29 11:36:16 +0530

    commited

--- epam/src/epam/p22.java
+++ epam/src/epam/p22.java
@@ -1,5 +1,42 @@
 package epam;
 
 public class p22 {
+	public static void main(String args[])
+	{
+		if(args.length==0)
+		{
+			System.out.println("Please enter an integer number");
+		}
+		else
+		{
+			int a=Integer.parseInt(args[0]);
+			int c=0;
+			if(a==0||a==1)
+			{
+				System.out.println(a+"is neither prime nor composite");
+			}
+			else
+			{
+			for(int i = 2; i <= a/2; i++)
+			{
+				if(a % i == 0)
+				{
+					c++;
+					
+				}
+			}
+			if (c>=1)
+			{
+			System.out.println(args[0] + " is not a prime number.");
+			}
+			else
+			{
+			System.out.println(args[0] + " is  a prime number.");
+			}
+			
+		}
+	}
+}
+	
 
 }
